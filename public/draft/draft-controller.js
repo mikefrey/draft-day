6c2636bec2df3c676fc93f0a84ccd6abcecef657
Mike Frey <frey.mike@gmail.com>
2014-08-02 22:57:06 -0500

    fixes picks showing in rounds

--- public/draft/draft-controller.js
+++ public/draft/draft-controller.js
@@ -1,37 +1,40 @@
 angular.module('draftDay')
   .controller('DraftController', function(picks, Picks, $routeParams) {
 
-    // var picks = Picks.query({ offense: $routeParams.side.toLowerCase() === 'offense' },
-    //   function() {
+    picks.$promise.then(function() {
 
-    // find current pick
-    for (var i = 0; i < picks.length; i++) {
-      if (!picks[i].player) {
-        this.currentPick = picks[i].number
-        break
+      console.log('PICKS', picks)
+
+      // find current pick
+      this.currentPick = 1
+      for (var i = 0; i < picks.length; i++) {
+        if (!picks[i].player) {
+          this.currentPick = picks[i].number
+          break
+        }
       }
-    }
 
-    this.current = {}
-    this.prev = {}
-    this.next = {}
+      this.current = {}
+      this.prev = {}
+      this.next = {}
 
-    // get current round
-    this.current.round = (((this.currentPick - 1) / 10)|0) + 1
-    var cStart = ((this.currentPick / 10)|0) * 10
-    this.current.picks = picks.slice(cStart, cStart+10)
+      // get current round
+      this.current.round = (((this.currentPick - 1) / 10)|0) + 1
+      var cStart = ((this.currentPick / 10)|0) * 10
+      this.current.picks = picks.slice(cStart, cStart+10)
 
-    // get prev round
-    this.prev.round = this.current.round - 1
-    var pStart = cStart - 10
-    this.prev.picks = pStart < 0 ? fillRound() : picks.slice(pStart, pStart+10)
+      // get prev round
+      this.prev.round = this.current.round - 1
+      var pStart = cStart - 10
+      this.prev.picks = pStart < 0 ? fillRound() : picks.slice(pStart, pStart+10)
 
-    // get next round
-    this.next.round = this.current.round + 1
-    var nStart = cStart + 10
-    this.next.picks = nStart > picks.length-1 ? fillRound() : picks.slice(nStart, nStart+10)
+      // get next round
+      this.next.round = this.current.round + 1
+      var nStart = cStart + 10
+      this.next.picks = nStart > picks.length-1 ? fillRound() : picks.slice(nStart, nStart+10)
 
-    console.log(this)
+      console.log(this)
+    }.bind(this))
 
     // })
 
