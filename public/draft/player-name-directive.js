323741a6df04e3d9ca1926dad8e0584da774c31c
Mike Frey <frey.mike@gmail.com>
2014-08-17 13:49:51 -0500

    fixes on the clock switch

--- public/draft/player-name-directive.js
+++ public/draft/player-name-directive.js
@@ -11,21 +11,26 @@ angular.module('draftDay')
       link: function(scope, element, attrs) {
         var pick = scope.pick
 
-        if (pick.player && pick.player.firstname) {
-          scope.text = pick.player.firstname + ' ' + pick.player.lastname
-        }
-        else if (scope.currentPick == pick.number) {
-          scope.text = 'On The Clock'
-        }
-        else if (scope.currentPick+1 == pick.number) {
-          scope.text = 'On Deck'
-        }
-        else if (scope.currentPick+2 == pick.number) {
-          scope.text = 'In The Hole'
-        }
-        else {
-          scope.text = ''
-        }
+        scope.$watch('currentPick', function(currentPick) {
+
+          if (pick.player && pick.player.firstname) {
+            scope.text = pick.player.firstname + ' ' + pick.player.lastname
+          }
+          else if (currentPick == pick.number) {
+            scope.text = 'On The Clock'
+          }
+          else if (currentPick+1 == pick.number) {
+            scope.text = 'On Deck'
+          }
+          else if (currentPick+2 == pick.number) {
+            scope.text = 'In The Hole'
+          }
+          else {
+            scope.text = ''
+          }
+
+        })
+
       }
     }
 
