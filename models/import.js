69f82405ba253d2eae6b785c851312ce2d2b1296
Mike Frey <frey.mike@gmail.com>
2019-08-20 18:08:36 -0500

    2019 updates

--- models/import.js
+++ models/import.js
@@ -13,16 +13,16 @@ async function start () {
 
 async function importTeams () {
   const teams = [
-    { id: 1,    abbrev: 'MF',  name: 'Dragons',        city: 'Mount Fuji',   slug: 'mount-fuji' },
-    { id: 2,    abbrev: 'GC',  name: 'Dark Knights',   city: 'Gotham City',  slug: 'gotham-city' },
-    { id: 3,    abbrev: 'WAS', name: 'Weasels',        city: 'Washington',   slug: 'washington' },
-    { id: 4,    abbrev: 'LL',  name: 'Goonies',        city: 'Leech Lake',   slug: 'leech-lake' },
-    { id: 5,    abbrev: 'BR',  name: 'Rubadubs',       city: 'Baton Rouge',  slug: 'baton-rouge' },
+    { id: 1,    abbrev: 'SC',  name: 'Outcasts',       city: 'Sin City',     slug: 'sin-city' },
+    { id: 2,    abbrev: 'LL',  name: 'Goonies',        city: 'Leech Lake',   slug: 'leech-lake' },
+    { id: 3,    abbrev: 'BR',  name: 'Rubadubs',       city: 'Baton Rouge',  slug: 'baton-rouge' },
+    { id: 4,    abbrev: 'SI', name: 'Godfabers',       city: 'Staten Island',slug: 'staten-island' },
+    { id: 5,    abbrev: 'HUT', name: 'Hell on Cleats', city: 'Hutchinson',   slug: 'hutch' },
     { id: 6,    abbrev: 'ML',  name: 'Green Backs',    city: 'Mille Lacs',   slug: 'mille-lacs' },
-    { id: 7,    abbrev: 'SC',  name: 'Outcasts',       city: 'Sin City',     slug: 'sin-city' },
-    { id: 8,    abbrev: 'CC',  name: 'Flash',          city: 'Central City', slug: 'central-city' },
-    { id: 9,    abbrev: 'ATL', name: 'Rednecks',       city: 'Atlanta',      slug: 'atlanta' },
-    { id: 10,   abbrev: 'HUT', name: 'Hell on Cleats', city: 'Hutchinson',   slug: 'hutch' },
+    { id: 7,    abbrev: 'WAS', name: 'Weasels',        city: 'Washington',   slug: 'washington' },
+    { id: 8,    abbrev: 'GC',  name: 'Dark Knights',   city: 'Gotham City',  slug: 'gotham-city' },
+    { id: 9,    abbrev: 'CC',  name: 'Flash',          city: 'Central City', slug: 'central-city' },
+    { id: 10,   abbrev: 'MF',  name: 'Dragons',        city: 'Mount Fuji',   slug: 'mount-fuji' },
   ]
 
   await Team.bulkCreate(teams)
